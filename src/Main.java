46fa3595e9c7a6f54c7e3fc4b70af0a8d0b59463
blortle <144735570+blortle-dev@users.noreply.github.com>
2025-02-12 09:58:24 -0600

    Added configuration

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
+        // CONFIGURATION
+        // Tune the variables to match your transect description.
+
+        final int transectWidth = 125; // One side of the transect width, in meters.
+        final double transectLength = 0.8; // Length of the transect, in kilometers.
+
         System.out.println("Starting program...");
-        AnalysisWindow analysisWindow = new AnalysisWindow(125, 0.8);
+        AnalysisWindow analysisWindow = new AnalysisWindow(transectWidth, transectLength);
     }
 }
